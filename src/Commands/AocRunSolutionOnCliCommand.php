f234cf2076a1ae44dd3b38f6c59661129448c06f
Mitchell de Roode <m-roode@outlook.com>
2024-10-09 20:40:17 +0200

    ready for tag v1.0.5

--- src/Commands/AocRunSolutionOnCliCommand.php
+++ src/Commands/AocRunSolutionOnCliCommand.php
@@ -4,6 +4,7 @@ namespace Mjderoode\AdventOfCodeHelper\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Collection;
 
 class AocRunSolutionOnCliCommand extends Command
 {
@@ -25,6 +26,20 @@ class AocRunSolutionOnCliCommand extends Command
         }
 
         $controller = app()->make("App\Http\Controllers\Year{$year}\Day{$day}Controller");
-        dd($controller->handle($part));
+        $result = $controller->handle($part);
+
+        if (is_string($result) || is_numeric($result)) {
+            $this->line((string) $result);
+        } elseif (is_array($result) || $result instanceof Collection) {
+            $this->line(json_encode($result, JSON_PRETTY_PRINT));
+        } elseif (is_object($result)) {
+            try {
+                $this->line(json_encode($result, JSON_PRETTY_PRINT));
+            } catch (\Exception $e) {
+                $this->line('Object of class '. get_class($result));
+            }
+        } else {
+            $this->line(var_export($result, true));
+        }
     }
 }
